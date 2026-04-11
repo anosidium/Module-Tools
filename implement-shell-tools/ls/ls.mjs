e89484fc2d08bd4aff18ad07491c76dc80020626
Ammad <anosidium@hotmail.com>
2026-04-11 12:50:58 +0100

    Implement -a flag

--- implement-shell-tools/ls/ls.mjs
+++ implement-shell-tools/ls/ls.mjs
@@ -2,8 +2,11 @@ import { program } from "commander";
 import { promises as fs } from "node:fs";
 import process from "node:process";
 
-program.name("ls").description("test").option("-1", "list one file per line").argument("<path>");
-
+program.name("ls");
+program.description("test");
+program.option("-1", "list one file per line");
+program.option("-a", "show hidden files");
+program.argument("<path>");
 program.parse();
 
 const argv = program.args;
@@ -13,14 +16,18 @@ if (argv.length != 1) {
   process.exit(1);
 }
 
-const path = argv[0];
 const options = program.opts();
+const path = argv[0];
 
 try {
   const stat = await fs.stat(path);
 
   if (stat.isDirectory()) {
-    const files = await fs.readdir(path);
+    let files = await fs.readdir(path);
+
+    if (!options.a) {
+      files = files.filter((file) => !file.startsWith("."));
+    }
 
     if (options["1"]) {
       for (const file of files) {
