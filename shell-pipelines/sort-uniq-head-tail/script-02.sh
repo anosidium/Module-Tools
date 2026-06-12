0e2571c71e92f54145f50fb4b8dae2a677a88ed6
Ammad <anosidium@hotmail.com>
2026-06-12 22:35:42 +0100

    Fix script-02.sh solution.

--- shell-pipelines/sort-uniq-head-tail/script-02.sh
+++ shell-pipelines/sort-uniq-head-tail/script-02.sh
@@ -6,4 +6,4 @@ set -euo pipefail
 # TODO: Write a command to output scores-table.txt, with lines sorted by the person's first score, descending.
 # The first line of your output should be "Basia London 22 9 6" (with no quotes).
 
-sort -k2 -nr scores-table.txt
+sort -k3,3nr scores-table.txt
