abb66cf3ba77d2d99bfbf5a6c7786ee70cad4705
Ammad <anosidium@hotmail.com>
2026-06-14 16:25:30 +0100

    Implement configurable cowsay animal.

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,2 +1,21 @@
 import cowsay
-import sys
+import argparse
+
+parser = argparse.ArgumentParser(prog="cow.py",
+                                 description="Print a message using a cowsay animal")
+                                 
+parser.add_argument("message")
+parser.add_argument("-a",
+                    "--animal",
+                    default="cow",
+                    help="Animal to use for the speech bubble.")
+
+args = parser.parse_args()
+
+animal_names = cowsay.char_names
+
+if args.animal not in animal_names:
+    parser.error("The specified animal is not found in the cowsay animal list.")
+
+animal_function = getattr(cowsay, args.animal)
+animal_function(args.message)
