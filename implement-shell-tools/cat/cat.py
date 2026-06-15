cf0e413268ccef0baa31732ee807627264aac0ac
Ammad <anosidium@hotmail.com>
2026-06-15 20:19:47 +0100

    Complete the implementation of Python's cat.

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 parser = argparse.ArgumentParser(prog="cat",
                                  description="simple cat clone")
@@ -18,3 +19,30 @@ parser.add_argument("-b",
 parser.add_argument("paths", nargs="+", help="file(s) to process")
 
 args = parser.parse_args()
+
+def process_line(line_number, line):
+    if args.number_nonblank:
+        if line == "\n":
+            print(line, end="")
+            return line_number
+        
+        print(f"\t{line_number}  {line}", end="")
+        return line_number + 1
+    
+    if args.number_all:
+        print(f"\t{line_number}  {line}", end="")
+        return line_number + 1
+    
+    print(line, end="")
+    return line_number
+
+for path in args.paths:
+    if os.path.isdir(path):
+        print(f"cat: {path}: Is a directory")
+        continue
+
+    with open(path, "r") as file:
+        line_number = 1
+        
+        for line in file:
+            line_number = process_line(line_number, line)
