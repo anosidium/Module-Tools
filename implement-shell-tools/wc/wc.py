6f5e3ddaf80a74540ad070c505c58e011b671121
Ammad <anosidium@hotmail.com>
2026-06-15 18:38:36 +0100

    Implement the basic program.

--- implement-shell-tools/wc/wc.py
+++ implement-shell-tools/wc/wc.py
@@ -5,22 +5,50 @@ parser = argparse.ArgumentParser(prog="wc",
 
 parser.add_argument("-w",
                     "--words",
-                    dest="words_count",
+                    dest="show_words",
                     action="store_true",
                     help="print word count")
 
 parser.add_argument("-l",
                     "--lines",
-                    dest="lines_count",
+                    dest="show_lines",
                     action="store_true",
                     help="print line count")
 
 parser.add_argument("-c",
                     "--bytes",
-                    dest="byte_count",
+                    dest="show_bytes",
                     action="store_true",
                     help="print byte count")
 
 parser.add_argument("path")
 
 args = parser.parse_args()
+
+if not args.show_lines and not args.show_words and not args.show_bytes:
+    args.show_lines = True
+    args.show_words = True
+    args.show_bytes = True
+
+with open(args.path, "rb") as file:
+    content = file.read()
+
+word_count = str(len(content.split()))
+line_count = str(content.count(b"\n"))
+byte_count = str(len(content))
+
+outputs = []
+
+if args.show_lines:
+    outputs.append(line_count)
+
+if args.show_words:
+    outputs.append(word_count)
+
+if args.show_bytes:
+    outputs.append(byte_count)
+
+outputs.append(args.path)
+
+output = "\t".join(outputs)
+print(output)
