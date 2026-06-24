f7f4ed24a47accd5fc794915d57ebfa7d9b3fe2f
Ammad <anosidium@hotmail.com>
2026-06-24 23:27:11 +0100

    Add suggestion for -a/--animal argument.

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,6 +1,8 @@
 import argparse
 import cowsay
 
+animal_names = cowsay.char_names
+
 parser = argparse.ArgumentParser(prog="cow.py",
                                  description="Print a message using a cowsay animal")
                                  
@@ -10,11 +12,10 @@ parser.add_argument("message",
 parser.add_argument("-a",
                     "--animal",
                     default="cow",
+                    choices=animal_names,
                     help="Animal to use for the speech bubble.")
 
 args = parser.parse_args()
-
-animal_names = cowsay.char_names
 
 if args.animal not in animal_names:
     parser.error("The specified animal is not found in the cowsay animal list.")
