d1f8599897c7bd5187aaf90b433a2952e2742e17
Ammad <anosidium@hotmail.com>
2026-06-28 21:17:30 +0100

    Completed "Type checking with mypy" exercise.

---
+++ prep exercises/Type checking with mypy exercise.py
@@ -0,0 +1,34 @@
+def open_account(balances: dict[str, float], name: str, amount: float) -> None:
+    balances[name] = amount
+
+def sum_balances(accounts: dict[str, float]) -> float:
+    total: float = 0
+
+    for name, pence in accounts.items():
+        print(f"{name} had balance {pence}")
+        total += pence
+
+    return total
+
+def format_pence_as_string(total_pence: float) -> str:
+    if total_pence < 100:
+        return f"{total_pence}p"
+    
+    pounds = int(total_pence / 100)
+    pence = int(total_pence % 100)
+
+    return f"£{pounds}.{pence:02d}"
+
+balances: dict[str, float] = {
+    "Sima": 700,
+    "Linn": 545,
+    "Georg": 831,
+}
+
+open_account(balances, "Tobi", 9.13)
+open_account(balances, "Olya", 7.13)
+
+total_pence = sum_balances(balances)
+total_string = format_pence_as_string(total_pence)
+
+print(f"The bank accounts total {total_string}")
